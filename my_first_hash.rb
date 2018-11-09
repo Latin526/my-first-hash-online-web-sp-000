c6882e6e616e2219e121190c7c585e5650a00162
Yasmin Torres <yasmintorres90@hotmail.com>
2018-11-09 00:17:43 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -13,6 +13,6 @@ def my_hash
  
  def adding
    shipping_manifest = {"whale bone corset" => 5, "porcelane vases" => 2, "oil paintings" => 3}
-   shipping_manifest["pearl necklace"] = 1 
+   shipping_manifest["gun powder"] = 4
    shipping_manifest
  end
